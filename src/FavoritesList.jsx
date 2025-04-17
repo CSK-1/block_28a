3c4c13dc3ea053f1de1e23d79b1733aac4e1224d
Chase K <chasekmbll@gmail.com>
2025-04-17 17:48:47 -0500

    removing old comment

--- src/FavoritesList.jsx
+++ src/FavoritesList.jsx
@@ -6,7 +6,6 @@ function FavoritesList({favRecipes, setFavRecipes}){
             recipes.map((recipe)=>
                 <div key={recipe.idMeal}>
                     <img src={recipe?.strMealThumb} style={{height: "400px"}}/>
-                    {/* question mark there is shorthand for "if image is not there, do not render it" */}
                     <h2>{recipe.strMeal}</h2>
                 </div>
             )
